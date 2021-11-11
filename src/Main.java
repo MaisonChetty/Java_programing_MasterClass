bb21286bc6fbe12320aeeaee25005b9ed402ea69
MaisonChetty <79193288+MaisonChetty@users.noreply.github.com>
2021-11-11 09:15:58 +0200

    fixing

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        water type=new water();
+        type.setDragonType();
+        earth type1=new earth();
+        type1.Dragontype();
+        air type2=new air();
     }
 }
